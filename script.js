6a07d185cdcc6b88838863aec34fbea4f4cdcd25
TxSaikia <155208762+TxSaikia@users.noreply.github.com>
2024-12-20 15:23:59 +0530

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-// Example: Click event for an image
-document.querySelectorAll('.photo img').forEach((img) => {
-    img.addEventListener('click', () => {
-        alert('You clicked on: ' + img.alt);
-    });
-});
